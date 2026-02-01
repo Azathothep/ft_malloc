9e94bac1dcc0c12530c656271ec7a46249faae14
Azathothep <felixbelthoise@yahoo.fr>
2026-02-01 14:13:22 +0100

    Documented test main

--- test/main.c
+++ test/main.c
@@ -11,162 +11,156 @@
  #include "ft_malloc.h"
 #endif
 
-void process_allocation(void *a[], int index) {	
-	int i = 0;
-	while (i < index - 1){
-		size_t size = (i % 20 * 64) + 1;
-    		a[i] = malloc(size);
-    		i++;
-	}
-
-	a[index - 1] = malloc(5000);
-}
-
-void process_free(void *a[], int index) {
-	int i = 0;
-	while (i < index) {
-		free(a[i]);
-		i++;
-	}
-}
-
-void	lst_alloc(t_list **begin_lst, size_t size) {
-	char *alloc = malloc(size);	
+void	lst_add_allocation(t_list **BeginLst, size_t AllocationSize) {
+	char *Allocation = malloc(AllocationSize);	
 	size_t i = 0;
-	while (i < size) {
-		alloc[i] = 0xFF;
+	while (i < AllocationSize) {
+		Allocation[i] = 0xFF;
 		i++;
 	}
 
-	t_list *list = ft_lstnew(alloc);
-	ft_lstadd_front(begin_lst, list);
+	t_list *List = ft_lstnew(Allocation);
+	ft_lstadd_front(BeginLst, List);
 }
 
-void	lst_free(t_list **begin_lst, int index) {
-	t_list *lst = *begin_lst;
+void	lst_free_allocation(t_list **BeginLst, int LstIndex) {
+	t_list *Lst = *BeginLst;
 
-	if (index == 0)
+	if (LstIndex == 0)
 	{
-		*begin_lst = lst->next;
-		ft_lstdelone(lst, &free);
+		*BeginLst = Lst->next;
+		ft_lstdelone(Lst, &free);
 		return;
 	}
 
 	int i = 0;
-	while (lst != NULL && i < (index - 1)) {
-		lst = lst->next;
+	while (Lst != NULL && i < (LstIndex - 1)) {
+		Lst = Lst->next;
 		i++;
 	}
 
-	if (lst == NULL)
+	if (Lst == NULL)
 		return;
 
-	t_list *to_free = lst->next;
-	if (to_free == NULL) {
+	t_list *ToFree = Lst->next;
+	if (ToFree == NULL) {
 		return;
 	}
 
-	lst->next = to_free->next;
-	ft_lstdelone(to_free, &free);
+	Lst->next = ToFree->next;
+	ft_lstdelone(ToFree, &free);
 }
 
-void random_allocations(int maxAlloc, size_t maxAllocSize) {
-	t_list *begin_lst = NULL;
+// The test is divided into multiple steps:
+// 1) Allocate MaxNumberOfAllocations allocations, each one of a random size between 0 and MaxAllocationSize
+// 2) Randomly free 1/3rd of the MaxNumberOfAllocations
+// 3) Allocate again, up to 2/3rd of the MaxNumberOfAllocations
+// 4) Free again randomly 2/3rd of the MaxNumberOfAllocations
+// 5) Allocate again, up to the MaxNumberOfAllocations
+// 6) Finally, free everything.
+// This process is designed to stress test the defragmentation process, by freeing only some part of memory and re-allocating it just after
+void test_malloc_and_free(int MaxNumberOfAllocations, size_t MaxAllocationSize) {
+	t_list *BeginLst = NULL;
 
 	int i = 0;
-	int third = maxAlloc / 3;
-	int twoThird = maxAlloc - third;
+	int OneThirdAllocationsCount = MaxNumberOfAllocations / 3;
+	int TwoThirdAllocationsCount = MaxNumberOfAllocations - OneThirdAllocationsCount;
 
-	// allocating maxAlloc
-	while (i < maxAlloc) {
-		size_t size = (rand() % maxAllocSize) + 1;
-		lst_alloc(&begin_lst, size);
+// 1) Allocate MaxNumberOfAllocations allocations, each one of a random size between 0 and MaxAllocationSize
+	while (i < MaxNumberOfAllocations) {
+		size_t size = (rand() % MaxAllocationSize) + 1;
+		lst_add_allocation(&BeginLst, size);
 		i++;
 	}
 
-	// freeing two-third of allocations, goes to 1-third
+// 2) Randomly free 1/3rd of the MaxNumberOfAllocations
 	i = 0;
-	while (i < twoThird) {
-		int index = (rand() % (maxAlloc - i));
-		lst_free(&begin_lst, index);
+	while (i < TwoThirdAllocationsCount) {
+		int index = (rand() % (MaxNumberOfAllocations - i));
+		lst_free_allocation(&BeginLst, index);
 		i++;
 	}
 
-	// re-allocating one-third, goes to 2-third
+// 3) Allocate 1/3rd, up to 2/3rd of the MaxNumberOfAllocations
 	i = 0;
-while (i < third) {
-		size_t size = (rand() % maxAllocSize) + 1;
-		lst_alloc(&begin_lst, size);
+	while (i < OneThirdAllocationsCount) {
+		size_t size = (rand() % MaxAllocationSize) + 1;
+		lst_add_allocation(&BeginLst, size);
 		i++;
 	}
 
-	// re-freeing one-third, goes to 1-third
+// 4) Free again randomly 2/3rd of the MaxNumberOfAllocations
 	i = 0;
-	while (i < third) {
-		int index = (rand() % (twoThird - i));
-		lst_free(&begin_lst, index);
+	while (i < OneThirdAllocationsCount) {
+		int index = (rand() % (TwoThirdAllocationsCount - i));
+		lst_free_allocation(&BeginLst, index);
 		i++;
 	}
 
-	// re-allocating to max, goes to maxAlloc
-	i = ft_lstsize(begin_lst);
-	while (i < maxAlloc) {
-		size_t size = (rand() % maxAllocSize) + 1;
-		lst_alloc(&begin_lst, size);
+// 5) Allocate 2/rd, up to the MaxNumberOfAllocations
+	i = ft_lstsize(BeginLst);
+	while (i < TwoThirdAllocationsCount) {
+		size_t size = (rand() % MaxAllocationSize) + 1;
+		lst_add_allocation(&BeginLst, size);
 		i++;
 	}
 
-	// freeing everything
-	ft_lstclear(&begin_lst, &free);
+// 6) Finally, free everything.
+	ft_lstclear(&BeginLst, &free);
 }
 
+// Arguments: [Max number of allocations] [Max allocation size] [Loops] [Seed]
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
 
-	unsigned int seed = time(NULL);
-	int 	n = 1;
-	int		maxAlloc = 10000; //100;
-	size_t 	maxAllocSize = 2048; //20480;
+	int		MaxNumberOfAllocations = 10000; //100;
+	size_t 	MaxAllocationSize = 2048;
+	int 	Loops = 1;
+	unsigned int Seed = time(NULL);
 
 	if (argc > 1)
-		seed = atoi(argv[1]);
+		MaxNumberOfAllocations = atoi(argv[1]);
 
 	if (argc > 2)
-		n = atoi(argv[2]);
+		MaxAllocationSize = atoi(argv[2]);
 
 	if (argc > 3)
-		maxAlloc = atoi(argv[3]);
+		Loops = atoi(argv[3]);
 
 	if (argc > 4)
-		maxAllocSize = atoi(argv[4]);
+		Seed = atoi(argv[4]);
 
 	char seedchar[32];
 	ft_bzero(seedchar, 32);
-	sprintf(seedchar, "%u", seed);
-	srand(seed);
+	sprintf(seedchar, "%u", Seed);
+	srand(Seed);
 
+	printf("SEED: %s\n", seedchar);
+	
 	struct timeval t0, t1;
-	float mallocTotalTime = 0;
+	float MallocTotalTime = 0;
 
 	int i = 0;
-	while (i < n) {
+	while (i < Loops) {
+		printf("Loops %d / %d...\r", i, Loops);
+		fflush(stdout);
 		gettimeofday(&t0, NULL);
-		random_allocations(maxAlloc, maxAllocSize);
+		test_malloc_and_free(MaxNumberOfAllocations, MaxAllocationSize);
 		gettimeofday(&t1, NULL);
 		
 		float deltaTime = t1.tv_sec - t0.tv_sec + 1E-6 * (t1.tv_usec - t0.tv_usec);
-		mallocTotalTime += deltaTime;		
+		MallocTotalTime += deltaTime;		
 		
 		i++;
 	}
 
-	float mallocAverageTime = mallocTotalTime / i;
+	float MallocAverageTime = MallocTotalTime / i;
 
-	printf("MALLOC - Total time: %.2g seconds (%f)\n", mallocTotalTime, mallocAverageTime);
-
-	printf("SEED: %s\n", seedchar);
+	printf("Total time: %d loops in %.2g seconds (%fs / loop)\n", Loops, MallocTotalTime, MallocAverageTime);
+	printf("Max number of allocations: %d\n", MaxNumberOfAllocations);
+	printf("Max allocation size: %lu\n", MaxAllocationSize);
   
 	return 0;
 }
